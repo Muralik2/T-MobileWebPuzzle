516122f3a64653eeaaee6bb373380053f529dca4
GeeksforGeeks <muralik274@gmail.com>
2024-02-22 12:18:27 -0600

    Undo add and remove from reading list

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
-
+import { FormControl } from '@angular/forms';
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { of } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-describe('ProductsListComponent', () => {
+describe('BookSearchComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
 
@@ -24,4 +26,31 @@ describe('ProductsListComponent', () => {
   it('should create', () => {
     expect(component).toBeDefined();
   });
+  it('should initialize with an empty search term', () => {
+    expect(component.searchForm.value.term).toEqual('');
+  });
+
+  it('should have a search input element', () => {
+    const searchInput = fixture.nativeElement.querySelector('#searchInput');
+    expect(searchInput).toBeTruthy();
+  });
+
+  it('should set search term when calling searchExample', () => {
+    component.searchExample();
+    expect(component.searchTerm).toEqual('javascript');
+  });
+
+  
+    it('should debounce and call searchBooks', (done) => {
+      spyOn(component, 'searchBooks');
+  
+      // Trigger value changes
+      component.searchForm.get('term').setValue('javascript');
+  
+      setTimeout(() => {
+        expect(component.searchBooks).toHaveBeenCalledWith();
+        done();
+      }, 501); // Debounce time is 500 ms, so we wait a bit more than that
+    });
+
 });
